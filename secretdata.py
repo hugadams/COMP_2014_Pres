54a6fd1d9f4b97cb2ca33bbbc48d16a98ce3b454
hugadams <hugadams@gwmail.gwu.edu>
2014-11-20 14:24:48 -0500

    first commit

---
+++ secretdata.py
@@ -0,0 +1,5 @@
+from skimage.data import lena
+from IPython.display import Image
+
+secret = lena()
+appropriate_response = Image(url='http://www.reactiongifs.com/wp-content/uploads/2012/12/friday-damn.gif');
