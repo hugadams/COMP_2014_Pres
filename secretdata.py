67a96d9f73f5b59565e1693daaa1e373a6f8d6d7
hugadams <hugadams@gwmail.gwu.edu>
2014-11-21 12:44:03 -0500

    bam

--- secretdata.py
+++ secretdata.py
@@ -1,5 +1,6 @@
-from skimage.data import lena
 from IPython.display import Image
+from skimage.io import imread
+from skimage.color import gray2rgb
 
-secret = lena()
+secret = gray2rgb(imread('images/dreamy.jpg'))
 appropriate_response = Image(url='http://www.reactiongifs.com/wp-content/uploads/2012/12/friday-damn.gif');
